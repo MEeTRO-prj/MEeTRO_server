2fc666578795d86ab8600e20a034ffad486b9e85
Kiyoshi <kt.kiyoshi@gmail.com>
2014-10-05 18:27:06 +0900

    Add update_user.php for different registration

--- new_user.php
+++ new_user.php
@@ -3,33 +3,32 @@ include_once './DbConnect.php';
 
 function createNewUser() {
   if (isset($_POST["USER_NAME"]) && $_POST["USER_NAME"] != "") {
-       // response array for json
-       $response = array();
-       $regi_id = $_POST["REGI_ID"];
-       $user_name = $_POST["USER_NAME"];
+   // response array for json
+   $response = array();
+   $regi_id = $_POST["REGI_ID"];
+   $user_name = $_POST["USER_NAME"];
 
-       // mysql query
-       $query = "INSERT INTO USER (REGI_ID, USER_NAME) VALUES('$regi_id', '$user_name')";
-       $result = mysql_query($query) or die(mysql_error());
-       if ($result) {
-              $response["error"] = false;
-              $response["message"] = "User created successfully!";
-              $selected = mysql_query("SELECT USER_ID FROM USER WHERE REGI_ID = '$regi_id' ORDER BY USER_ID DESC LIMIT 1");
-              while ($row = mysql_fetch_array($selected)) {
-                      $response["user_id"] = $row["USER_ID"];
-              }
-       } else {
-              $response["error"] = true;
-              $response["message"] = "Failed to create user!";
-       }
- } else {
+   // mysql query
+   $query = "INSERT INTO USER (REGI_ID, USER_NAME) VALUES('$regi_id', '$user_name')";
+   $result = mysql_query($query) or die(mysql_error());
+   if ($result) {
+    $response["error"] = false;
+    $response["message"] = "User created successfully!";
+    $selected = mysql_query("SELECT USER_ID FROM USER WHERE REGI_ID = '$regi_id' ORDER BY USER_ID DESC LIMIT 1");
+    while ($row = mysql_fetch_array($selected)) {
+      $response["user_id"] = $row["USER_ID"];
+    }
+  } else {
+    $response["error"] = true;
+    $response["message"] = "Failed to create user!";
+  }
+} else {
  $response["error"] = true;
  $response["message"] = "User name is missing!";
- }
-
+}
 // echo json response
-   echo json_encode($response);
- }
+echo json_encode($response);
+}
 
 createNewUser();
 ?>
---
+++ update_user.php
@@ -0,0 +1,30 @@
+<?php
+include_once './DbConnect.php';
+
+function updateUser(){
+  if (isset($_POST["USER_ID"]) && $_POST["USER_ID"] != "") {
+   // response array for json
+   $response = array();
+   $user_id = $_POST["USER_ID"];
+   $regi_id = $_POST["REGI_ID"];
+
+   // mysql query
+   $query = "UPDATE USER SET REGI_ID = '$regi_id' WHERE USER_ID = '$user_id')";
+   $result = mysql_query($query) or die(mysql_error());
+   if ($result) {
+    $response["error"] = false;
+    $response["message"] = "User updated successfully!";
+  } else {
+    $response["error"] = true;
+    $response["message"] = "Failed to update user!";
+  }
+} else {
+ $response["error"] = true;
+ $response["message"] = "USER_ID is missing!";
+}
+// echo json response
+echo json_encode($response);
+}
+
+updateUser();
+?>
