b094c07b62db333853f71ec8d79db569610b43c5
kodamat <kt.kiyoshi@gmail.com>
2014-10-18 14:28:21 +0900

    Mod make_room.php

--- make_room.php
+++ make_room.php
@@ -14,12 +14,12 @@ function createNewUser() {
    $dest_st = $_POST["DEST_ST"];
 
    // mysql query
-   $query = "INSERT INTO ROOM (ROOM_ID, OWNER_ID, RIDE_DATE, RIDE_TIME, TIME_TYPE, RAILWAY_ID, RIDE_ST, DEST_ST, CAR_NUM, UPDATED_DATE, USE_FLG) VALUES(NULL, '$owner_id', '$ride_date', '$ride_time', '$time_type', '$railway_id', '$ride_st', '$dest_st', 1, SYSDATE(), '1')";
+   $query = "INSERT INTO ROOM (ROOM_ID, OWNER_ID, RIDE_DATE, RIDE_TIME, TIME_TYPE, RAILWAY_ID, RIDE_ST, DEST_ST, CAR_NUM, UPDATED_DATE, USE_FLG) VALUES(NULL, '$owner_id', '$ride_date', '$ride_time', '$time_type', '$railway_id', '$ride_st', '$dest_st', 1, SYSDATE(), '0')";
    $result = mysql_query($query) or die(mysql_error());
    if ($result) {
     $response["error"] = false;
     $response["message"] = "Room created successfully!";
-    $selected = mysql_query("SELECT ROOM_ID FROM ROOM WHERE OWNER_ID = '$owner_id' ORDER BY OWNER_ID DESC LIMIT 1");
+    $selected = mysql_query("SELECT ROOM_ID FROM ROOM WHERE OWNER_ID = '$owner_id' ORDER BY ROOM_ID DESC LIMIT 1");
     while ($row = mysql_fetch_array($selected)) {
       $response["room_id"] = $row["ROOM_ID"];
     }
