8600e6a5bd2c22f1d2a34d1159eb5c8905fadf92
Kiyoshi <kt.kiyoshi@gmail.com>
2014-10-23 22:15:08 +0900

    Mod make_room.php for adding MEMBER

--- make_room.php
+++ make_room.php
@@ -16,6 +16,7 @@ function createNewUser() {
    // mysql query
    $query = "INSERT INTO ROOM (ROOM_ID, OWNER_ID, RIDE_DATE, RIDE_TIME, TIME_TYPE, RAILWAY_ID, RIDE_ST, DEST_ST, CAR_NUM, UPDATED_DATE, USE_FLG) VALUES(NULL, '$owner_id', '$ride_date', '$ride_time', '$time_type', '$railway_id', '$ride_st', '$dest_st', 1, SYSDATE(), '0')";
    $result = mysql_query($query) or die(mysql_error());
+
    if ($result) {
     $response["error"] = false;
     $response["message"] = "Room created successfully!";
@@ -23,6 +24,9 @@ function createNewUser() {
     while ($row = mysql_fetch_array($selected)) {
       $response["room_id"] = $row["ROOM_ID"];
     }
+    $room_id = $row["ROOM_ID"];
+    $query2 = "INSERT INTO MEMBER (MEMBER_ID, ROOM_ID, OWNER_ID, RIDE_ST, RIDE_TIME) VALUES(NULL, '$room_id' '$owner_id', '$ride_st', '$ride_time')";
+    $result2 = mysql_query($query) or die(mysql_error());
   } else {
     $response["error"] = true;
     $response["message"] = "Failed to create room!";
