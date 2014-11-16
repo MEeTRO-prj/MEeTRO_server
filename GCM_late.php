089305089334322546c8f45f7255156e1d6b6ee0
Takahiro Kodama <kt.kiyoshi@gmail.com>
2014-11-16 20:13:25 +0900

    Add GCM_late.php

--- GCM_late.php
+++ GCM_late.php
@@ -3,7 +3,6 @@ include_once './DbConnect.php';
 
 $registatoin_id = array();
 $room_id = $_POST["ROOM_ID"];
-$owner_id = $_POST["OWNER_ID"];
 $user_name = $_POST["USER_NAME"];
 
 // DBからOwnerのRegistrationIdを取得する
@@ -11,14 +10,13 @@ $query = "SELECT * FROM ROOM R JOIN USER U ON R.OWNER_ID = U.USER_ID WHERE R.ROO
 $result = mysql_query($query);
 while ($row = mysql_fetch_array($result)) {
     array_push($registatoin_id, $row["REGI_ID"]);
-    //$regi_id = $row["REGI_ID"];
 }
 
 //送りたいメッセージ
 $notificationMsg = "You got a message from MEeTRO.";
 $roomId   = $room_id;
 $msgTitle = "MEeTRO's information";
-$msgContext = $user_name." joined your ROOM[".$room_id."].";
+$msgContext = $user_name." is late on room [".$room_id."].";
 
 $response = sendNotification(
                GOOGLE_API_KEY,
